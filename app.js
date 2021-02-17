2773d3afc12f6d406a028ea4507ff343d703d739
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-02-17 00:39:48 -0300

    Despliegue app -2021-02-17

--- app.js
+++ app.js
@@ -13,25 +13,29 @@ console.log(__dirname);
 // Servir contenido estatico
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home", {
-    nombre: "Fernando Herrera",
-    titulo: "Curso de Node",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.render("home", {
+//     nombre: "Fernando Herrera",
+//     titulo: "Curso de Node",
+//   });
+// });
 
-app.get("/generic", (req, res) => {
-  res.render("generic", {
-    nombre: "Fernando Herrera",
-    titulo: "Curso de Node",
-  });
-});
+// app.get("/generic", (req, res) => {
+//   res.render("generic", {
+//     nombre: "Fernando Herrera",
+//     titulo: "Curso de Node",
+//   });
+// });
 
-app.get("/elements", (req, res) => {
-  res.render("elements", {
-    nombre: "Fernando Herrera",
-    titulo: "Curso de Node",
-  });
+// app.get("/elements", (req, res) => {
+//   res.render("elements", {
+//     nombre: "Fernando Herrera",
+//     titulo: "Curso de Node",
+//   });
+// });
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "public/index.html");
 });
 
 app.listen(port, () => {
